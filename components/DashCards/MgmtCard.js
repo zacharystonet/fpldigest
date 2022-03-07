e1f77a98061fa6442ba9d98188991141255e7a0b
zacharystonet <zacharystonet@gmail.com>
2022-03-06 22:08:52 -0500

    remove team name for now

--- components/DashCards/MgmtCard.js
+++ components/DashCards/MgmtCard.js
@@ -30,7 +30,7 @@ function MgmtCard() {
         <div className="flex bg-white shadow-lg rounded-sm border border-slate-200">
             <div className="px-5 pt-5">
                 <div className="font-bold text-xl mb-2">Team Summary</div>
-                <p className="text-gray-700 text-base">Name: {teamName} </p>
+                {/*<p className="text-gray-700 text-base">Name: {teamName} </p>*/}
                 <p className="text-gray-700 text-base">Total points: {overallPoints}</p>
                 <p className="text-gray-700 text-base">Current GW points: {currentGWPoints}</p>
             </div>
